7d35e478d7fd69ac5332af2980c0f34c12757a90
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-05 11:29:43 +0530

    Added package1

---
+++ package1/One.java
@@ -0,0 +1,38 @@
+/*In this example we are trying to access the files form the package using command line*/
+
+/*
+C:\Users\ABHAY>d:
+
+D:\>cd package1
+
+D:\package1>javac One.java
+
+D:\package1>java One
+Error: Could not find or load main class One
+
+D:\package1>java One.java
+Error: Could not find or load main class One.java
+
+D:\package1>java package1.One
+Error: Could not find or load main class package1.One
+
+D:\package1>set classpath=%classpath%;d:\;.;
+
+D:\package1>java package1.One
+6
+
+D:\package1>
+*/
+
+package package1;
+
+class One{
+void add(int a){
+a++;
+System.out.println(a);}
+
+public static void main(String[] ar){
+One b = new One();
+b.add(5);
+}
+}
