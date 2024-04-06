1a17cf5c70bbf9ff999894ddb53dbd7c28dfb4f1
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-06 11:23:35 +0530

    Added Public and Private access modifiers

--- Intermediate java/Daughter.java
+++ Intermediate java/Daughter.java
@@ -2,7 +2,7 @@
 
 class Mother{
 
-void job(){
+private void job(){
 System.out.println("has own startup");
 }
 
@@ -18,15 +18,23 @@ System.out.println("flat in sambhajinagar");
 
 class Daughter extends Mother{
 
+void myjob(){
+System.out.println("Working in software sector");
+}
+
+public void property(){
+System.out.println("Owns a mercedes");
+}
+
 public static void main(String[] ar){
 
 Mother m1 = new Mother();
-m1.job();
+//m1.job();
 m1.property();
 m1.hobby();
 
 Daughter d1 = new Daughter();
-d1.job();
+d1.myjob();
 d1.property();
 d1.hobby();
 
