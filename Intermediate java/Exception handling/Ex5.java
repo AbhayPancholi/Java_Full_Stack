0edd053d88779732337fb078acdec3252b6f2808
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-10 10:45:13 +0530

    Added Ex5 file

---
+++ Intermediate java/Exception handling/Ex5.java
@@ -0,0 +1,33 @@
+//two numbers 
+
+class Ex5{
+
+static void div(int a, int b){
+try{
+System.out.println(a/b);}
+catch(ArithmeticException e){
+System.out.println("2nd value must be a non zero number");}}
+
+public static void main(String[] args){
+
+int a = 0;
+int b = 0;
+
+try{
+String s1 = args[0];
+String s2 = args[1];
+
+a = Integer.parseInt(s1);
+b = Integer.parseInt(s2);
+
+System.out.println(a+b);
+//System.out.println(a/b);
+
+}catch(ArrayIndexOutOfBoundsException e){
+System.out.println("Please enter any two values.\nWe got an exception: "+e);}
+catch(NumberFormatException e1){
+System.out.println("Please enter only numbers.\nWe got an exception: "+e1);}
+catch(ArithmeticException e2){
+System.out.println("2nd values must be a non zero number.\nWe got an exception: "+e2);}
+div(a,b);
+}}
