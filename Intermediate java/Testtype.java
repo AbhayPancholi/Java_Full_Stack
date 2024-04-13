d17f097a0c0a66fbb69e1809a6263f265f771fb6
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-13 12:15:38 +0530

    Modified Testtype.java file

--- Intermediate java/Testtype.java
+++ Intermediate java/Testtype.java
@@ -2,7 +2,13 @@ class Testtype{
 public static void main(String[] ar){
 
 byte b1 = 12;
+System.out.println("b1 is: "+b1);
 byte b2 = 13;
-byte sum = b1+b2;//this line will cause an error as the sum of two bytes //becomes an integer, the addition could not take place as byte value is //goes above 128 and the range of bytes is -128 to 127.
+System.out.println("b2 is: "+b2);
+//byte sum = b1+b2;
+//this line will cause an error as the sum of two bytes becomes an //integer, the addition could not take place as byte value is goes above //128 and the range of bytes is -128 to 127.
+
+int sum = b1+b2;
+System.out.println("The sum of b1 and b2 is: "+sum);
 }
 }
