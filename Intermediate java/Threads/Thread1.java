ccf3624124a0ffa155d0f73815d77f63a038f821
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-15 11:12:32 +0530

    Modified thread1 file

--- Intermediate java/Threads/Thread1.java
+++ Intermediate java/Threads/Thread1.java
@@ -1,6 +1,12 @@
 class Thread1{
+
+static{
+System.out.println("Static block: "+Thread.currentThread().getName());
+System.out.println(Thread.currentThread().getPriority());
+}
+
 public static void main(String[] ar){
-System.out.println(Thread.currentThread().getName());
+System.out.println("Main(): "+Thread.currentThread().getName());
 System.out.println(Thread.currentThread().getPriority());
 }
 }
