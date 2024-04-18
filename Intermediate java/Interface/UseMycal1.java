0c126a6a20ed3676396a6d8fce9066ce34031f9e
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-18 16:37:25 +0530

    Solved bug and added file

--- Intermediate java/Interface/UseMycal1.java
+++ Intermediate java/Interface/UseMycal1.java
@@ -10,6 +10,10 @@ public abstract void add(int a, int b);
 class UseMycal1{
 
 public static void main(String[] ar){
+<<<<<<< Updated upstream
 Mycal1 m1 = (int a, int b) -> System.out.println();
+=======
+Mycal1 m1 = (int a, int b) -> System.out.println(a+b);
+>>>>>>> Stashed changes
 m1.add(12, 23);}
 }
