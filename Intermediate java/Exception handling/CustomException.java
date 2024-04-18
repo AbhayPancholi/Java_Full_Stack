a78dc1df9e80b694ab6657cdd43c332bc950210a
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-18 10:26:55 +0530

    Added custom exception file

---
+++ Intermediate java/Exception handling/CustomException.java
@@ -0,0 +1,14 @@
+//Custom exception
+
+class InvalidAgeException extends Exception{
+String ex;
+InvalidAgeException(String myex){
+System.out.println("InvalidAgeException object created");
+
+System.out.println("Invalid age please check" + this.ex);
+}
+
+public String toString(){
+return "This is an instance of InvalidAgeException: "+this.ex;
+}
+}
