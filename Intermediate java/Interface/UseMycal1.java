cfba75295cbe0e12c37ee71d64b9f1e7ca8a6d53
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-18 12:50:39 +0530

    Added lambda method

--- Intermediate java/Interface/UseMycal1.java
+++ Intermediate java/Interface/UseMycal1.java
@@ -7,13 +7,9 @@ public abstract void add(int a, int b);
 }
 
 
-class UseMycal1 implements Mycal1{
-
-public void add(int a, int b){
-System.out.println(a+b);
-}
+class UseMycal1{
 
 public static void main(String[] ar){
-Mycal1 m1 = new UseMycal1();
+Mycal1 m1 = (int a, int b) -> System.out.println();
 m1.add(12, 23);}
 }
