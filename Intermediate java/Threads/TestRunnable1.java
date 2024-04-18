753694bdc1abfc268f14e4cbffa1a24836b4edf3
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-18 16:27:19 +0530

    Implementing Threads using runnable

---
+++ Intermediate java/Threads/TestRunnable1.java
@@ -0,0 +1,19 @@
+class TestRunnable1 implements Runnable{
+
+public void run(){
+
+System.out.println(Thread.currentThread().getName());
+
+}
+
+public static void main(String[] ar){
+
+TestRunnable t = new TestRunnable();
+Thread t1 = new Thread(t);
+t1.start();
+
+Thread t2 = new Thread(t);
+t2.start();
+}
+
+}
