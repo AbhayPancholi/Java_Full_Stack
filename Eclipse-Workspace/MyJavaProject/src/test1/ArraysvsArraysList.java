3e132e7d291259752891ebb4e0913e325284a303
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-24 11:06:54 +0530

    Addeding file for Arrays vs ArraysList

---
+++ Eclipse-Workspace/MyJavaProject/src/test1/ArraysvsArraysList.java
@@ -0,0 +1,38 @@
+package test1;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class ArraysvsArraysList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Normal arrays
+		int[] arr = new int[5];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		arr[4] = 5;
+		//Accessing array elements
+		System.out.println("the first element of array is: "+arr[0]);
+		System.out.println("The second element of array is: "+arr[1]);
+		System.out.println(Arrays.toString(arr));
+		
+		
+		//Array list 
+		//Creating an arraylist with initial capacity 5
+		ArrayList<Integer> arrayList = new ArrayList<Integer>(5);
+		//Adding elements to arrya list
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(3);
+		arrayList.add(4);
+		arrayList.add(5);
+		
+		System.out.println("the first element of the array list is: "+arrayList.get(0));
+		System.out.println("The second element of the arraylist is: "+arrayList.get(0));
+		
+		
+	}
+
+}
