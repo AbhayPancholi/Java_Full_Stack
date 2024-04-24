56b8a33e0d4328564521ef8ab97ce7650d466921
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-04-24 14:29:23 +0530

    Added new package collections1 in src folder

---
+++ Eclipse-Workspace/MyJavaProject/src/collections1/IntrotoCollections.java
@@ -0,0 +1,16 @@
+package collections1;
+
+//collection interface provides abstract of following basic operations
+//add(E element)
+//addAll()
+//size 
+//etc
+
+public class IntrotoCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
