972ea86da9ab19dda4151d9a91a45a1431b0f03f
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-05-02 11:52:01 +0530

    Added file for random access

---
+++ Eclipse-Workspace/MyJavaProject/src/io_in_java/RandomAccessFileDemo.java
@@ -0,0 +1,30 @@
+package io_in_java;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			RandomAccessFile raf=new RandomAccessFile("D:\\Study\\Java Full Stack\\Eclipse-Workspace\\MyJavaProject\\javacreatefilesample1.txt", "rw");
+			
+			//raf.writeUTF("Hello World");
+			
+			raf.seek(0);
+			System.out.println(" "+raf.readLine());
+			
+			raf.seek(10);
+			raf.writeUTF("This is an example \n Hello Workd");
+			
+			raf.seek(10);
+			System.out.println(" "+raf.readLine());
+			
+			raf.close();
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
