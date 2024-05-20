e4922801569707c8598c745d1c825b25c5c71022
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-05-20 14:37:43 +0530

    Added src folder contatining Addservlet23.java, web.xml and index.html in subfolders

---
+++ Eclipse-Workspace/WebProject_1/src/main/java/test23/Addservlet23.java
@@ -0,0 +1,53 @@
+package test23;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Addservlet23 extends HttpServlet{
+	public Addservlet23() {
+		
+		System.out.println("constructor of Addservlet23 is called");
+	}
+	public void init() {
+		
+		System.out.println("init method of Addservlet23 is called");
+	}
+	public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException{
+		System.out.println("get method called");
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		int a=Integer.parseInt(request.getParameter("t1"));
+		int b=Integer.parseInt(request.getParameter("t2"));
+		int c=a+b;
+		out.println(c);
+		out.println("end of the code");
+		 
+		
+		
+		
+	}
+public void doPost(HttpServletRequest request,HttpServletResponse response)throws IOException{
+		System.out.println("post method called");
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		int a=Integer.parseInt(request.getParameter("t1"));
+		int b=Integer.parseInt(request.getParameter("t2"));
+		int c=a+b;
+		out.println(c);
+		
+//doGet(request,response);		
+		
+		
+	}
+
+public void destroy() {
+	
+	System.out.println("destroy method of Addservlet23 is called when the code got a change to check wewww");
+}
+
+}
